efdb47d29ebab97a060a669cbd7febdbb63a024f
Arthur Wang <fnghwsj@gmail.com>
2018-04-01 14:49:09 +0800

    refactor: reconstruct Message struct

--- queen/src/scheme/message.go
+++ queen/src/scheme/message.go
@@ -1,11 +1,26 @@
 package scheme
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/CardInfoLink/log"
+)
 
 // Message represents a message which will be stored in the DB
 type Message struct {
-	Tag        string     `json:"tag" bson:"tag"`
-	Text       string     `json:"text" bson:"text"`
-	Desc       string     `json:"desc" bson:"desc"`
-	CreateTime *time.Time `json:"createTime" bson:"createTime"`
+	Tag        string      `json:"tag" bson:"tag"`
+	Payload    interface{} `json:"payload" bson:"payload"`
+	Desc       string      `json:"desc" bson:"desc"`
+	CreateTime *time.Time  `json:"createTime" bson:"createTime"`
+}
+
+// String returns a JSON string of the struct
+func (m *Message) String() string {
+	data, err := json.Marshal(m)
+	if err != nil {
+		log.Errorf("error caught when marshalling data: %s", err)
+	}
+
+	return string(data)
 }
